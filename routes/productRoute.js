a725d11f1136f8a62459793cc6c6fdaa2b48a95f
Mohammad Aamir <shayanmalik0786@gmail.com>
2022-03-23 13:54:04 +0530

    change route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({caseSensitive:true});
 const productCategory = require('../App/controllers/productCategory');
 
-router.post("/add",productCategory.addProduct);
-router.get("/get",productCategory.getProduct);
+router.post("/product",productCategory.addProduct);
+router.get("/product",productCategory.getProduct);
 
 module.exports = router;
